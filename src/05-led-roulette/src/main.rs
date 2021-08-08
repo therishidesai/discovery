a486d17e1dc090fe6d9f7adc2553398da4475a59
Rishi Desai <desai.rishi1@gmail.com>
2021-08-07 20:34:03 -0700

    working spinning light example

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -2,14 +2,20 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+	let mut curr_light = 0_usize;
+    loop {
+        leds[curr_light].on().ok();
+		leds[(curr_light+1) % 8].on().ok();
+        delay.delay_ms(50_u16);
+
+        leds[curr_light].off().ok();
+        delay.delay_ms(50_u16);
+		curr_light = (curr_light + 1) % 8;
+    }
 }
